d400a6136ba8e0b4d10a782ec569cb5b935880ef
craw0166 <craw0166@algonquinlive.com>
2018-04-25 22:04:37 -0400

    validation

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,103 +1,151 @@
 window.onload = init;
 
-function init(){
+function init() {
+  $("#submit-button").on("click", function(event) {
+    event.preventDefault();
 
-	$('#btn-submit').on('click', function(event){
-		event.preventDefault();
+		validate_required_inputs();
+		validate_email_inputs();
+  });
+}
 
-		required_check();
+function validate_required_inputs() {
+  var required_inputs = $(".required input, .required textarea");
+
+  $.each(required_inputs, function(input_index, req_input) {
+    var required_container = $(req_input).closest(".Contact-form");
+    var input_label = $(required_container)
+      .find(".input-label")
+      .text();
+
+    var feedback = $(required_container).find(".feedback");
+
+    var input_val = $(req_input)
+      .val()
+      .trim();
+
+    console.log("Label:" + input_label);
+    console.log("Value:" + input_val);
+
+    $(feedback).fadeOut(0);
+
+    if (input_val == "") {
+      console.log("empty value");
+
+      $(required_container).addClass("has-error");
+      $(required_container).removeClass("has-success");
+
+      $(feedback).text('A value is required for "' + input_label + '"');
+    } else {
+      console.log("entered value");
+
+      $(required_container).addClass("has-success");
+      $(required_container).removeClass("has-error");
+
+      $(feedback).text(
+        'The entered value for "' + input_label + '" is acceptable.'
+      );
+    }
+
+    $(feedback).fadeIn(300);
+  });
+
+  var required_checkbox_containers = $(".required-radio");
+
+  $.each(required_checkbox_containers, function(
+    req_check_container_index,
+    req_check_container
+  ) {
+    var required_checkboxes = $(req_check_container).find(
+      "input[type=radio]"
+    );
+    var checkbox_feedback = $(req_check_container).find(".feedback");
+    var checkbox_label = $(req_check_container)
+      .find(".input-label")
+      .text();
+    var check_counter = 0;
+
+    $(checkbox_feedback).fadeOut(0);
+
+    $.each(required_checkboxes, function(req_check_index, req_check) {
+      console.log($(req_check).prop("checked"));
+
+      if ($(req_check).prop("checked")) {
+        check_counter++;
+      }
+    });
+
+    if (check_counter > 0) {
+      // This is a success
+      $(req_check_container).addClass("has-success");
+      $(req_check_container).removeClass("has-error");
+      $(checkbox_feedback).text(
+        'The entered value for "' + checkbox_label + '" is acceptable.'
+      );
+    } else {
+      $(req_check_container).removeClass("has-success");
+      $(req_check_container).addClass("has-error");
+      $(checkbox_feedback).text(
+        'A value is required for "' + checkbox_label + '"'
+      );
+    }
+
+    $(checkbox_feedback).fadeIn(300);
 	});
 }
 
-function required_check() {
+function validate_email_inputs() {
+	var required_emails = $('.email input');
 
-	var required_inputs = $('.required input, .required textarea');
+	$.each(required_emails, function(index, req_input) {
+		var required_container = $(req_input).closest(".Contact-form");
+    var input_label = $(required_container)
+      .find(".input-label")
+      .text();
 
-	$.each(required_inputs, function(input_index, req_input) {
-		
-		var required_container = $(req_input).closest('.input-container');
-		var input_label = $(required_container).find('.label-text').text();
+    var feedback = $(required_container).find(".feedback");
 
-		var feedback = $(required_container).find('.feedback');
+    var input_val = $(req_input)
+      .val()
+      .trim();
 
-		var input_val = $(req_input).val().trim();
-
-		console.log("Label:"+input_label);
-		console.log("Value:"+input_val);
-
-		$(feedback).fadeOut(0);
-
-		if(input_val=='') {
-
-			console.log('empty value')
-
-			$(required_container).addClass('has-error');
-			$(required_container).removeClass('has-success');
-
-
-			$(feedback).text('A value is required for "'+input_label+'"');
-
-		} else {
-			console.log('entered value');
-
-			$(required_container).addClass('has-success');
-			$(required_container).removeClass('has-error');
-
-			$(feedback).text('The entered value for "'+input_label+'" is acceptable.');
+		if (input_val == '') {
+			return;
 		}
 
-		$(feedback).fadeIn(300);
+    console.log("Label:" + input_label);
+    console.log("Value:" + input_val);
 
+    $(feedback).fadeOut(0);
 
-	});
+    if (!validate_email(input_val)) {
+      console.log("invalid email");
 
-	var required_checkbox_containers = $('.required-checkbox');
+      $(required_container).addClass("has-error");
+      $(required_container).removeClass("has-success");
 
-	$.each(required_checkbox_containers, function(req_check_container_index, req_check_container){
+      $(feedback).text('A valid email address is required for "' + input_label + '"');
+    } else {
+      console.log("entered valid email");
 
-		var required_checkboxes = $(req_check_container).find('input[type=checkbox]');
-		var checkbox_feedback = $(req_check_container).find('.feedback');
-		var checkbox_label = $(req_check_container).find('.label-text').text();
-		var check_counter = 0;
+      $(required_container).addClass("has-success");
+      $(required_container).removeClass("has-error");
 
-		$(checkbox_feedback).fadeOut(0);
+      $(feedback).text(
+        'The entered value for "' + input_label + '" is acceptable.'
+      );
+    }
 
-		$.each(required_checkboxes, function(req_check_index, req_check){
-			console.log($(req_check).prop('checked'));
-
-			if($(req_check).prop('checked')) {
-				check_counter++;
-			}
-
-		});
-
-		if(check_counter>0) {
-			// This is a success
-			$(req_check_container).addClass('has-success');
-			$(req_check_container).removeClass('has-error');
-			$(checkbox_feedback).text('The entered value for "'+checkbox_label+'" is acceptable.');
-
-		} else {
-			$(req_check_container).removeClass('has-success');
-			$(req_check_container).addClass('has-error');
-			$(checkbox_feedback).text('A value is required for "'+checkbox_label+'"');
-
-		}
-
-		$(checkbox_feedback).fadeIn(300);
+    $(feedback).fadeIn(300);
 	});
 }
 
-function ValidateEmail(inputText) {
-	var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+function validate_email(inputText) {
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-	if(inputText.value.match(mailformat)){
-		document.form1.text1.focus();
-		return true;
-		}
-	else {
-		alert("Success!");
-		document.form1.text1.focus();
-	return false;
-	}
+  if (inputText.match(mailformat)) {
+    return true;
+  } else {
+    return false;
+  }
 }
